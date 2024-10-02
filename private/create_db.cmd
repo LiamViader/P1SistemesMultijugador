f3a6fc9548d3a53f8285f978190413167926cb8d
LiamViader <liam.viadr@gmail.com>
2024-10-02 13:14:08 +0200

    Update create_db.cmd

--- private/create_db.cmd
+++ private/create_db.cmd
@@ -1,3 +1,4 @@
+sqlite3 users.db "DROP TABLE IF EXISTS users;"
 sqlite3 users.db "CREATE TABLE IF NOT EXISTS `users` (`user_id` INTEGER PRIMARY KEY, `user_name` varchar(63), `user_password` varchar(255), `verification_token` varchar(255), is_verified INTEGER DEFAULT 0);"
 sqlite3 users.db "CREATE UNIQUE INDEX `user_name_UNIQUE` ON `users` (`user_name` ASC);"
 sqlite3 users.db "CREATE TABLE IF NOT EXISTS sessions (session_id varchar(64) PRIMARY KEY, user_name varchar(63) NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP);"
