94499e1be0b2532b1d14b6d87ee05b29d40b0e2d
LiamViader <liam.viadr@gmail.com>
2024-10-02 12:42:38 +0200

    sa

--- private/create_db.cmd
+++ private/create_db.cmd
@@ -1,2 +1,2 @@
-sqlite3 users.db "CREATE TABLE IF NOT EXISTS `users` (`user_id` INTEGER PRIMARY KEY, `user_name` varchar(63), `user_password` varchar(255), `verification_tokken` varchar(255), is_verified INTEGER DEFAULT 0);"
+sqlite3 users.db "CREATE TABLE IF NOT EXISTS `users` (`user_id` INTEGER PRIMARY KEY, `user_name` varchar(63), `user_password` varchar(255), `verification_token` varchar(255), is_verified INTEGER DEFAULT 0);"
 sqlite3 users.db "CREATE UNIQUE INDEX `user_name_UNIQUE` ON `users` (`user_name` ASC);"
