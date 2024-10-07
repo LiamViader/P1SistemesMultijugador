1c7725cc0e4b804c5a8aa6f8b71b302c13403101
Joel <60098544+Joelrumex@users.noreply.github.com>
2024-10-07 20:57:03 +0200

    Afegir captcha
    Falta verificar correctament

--- public/index.php
+++ public/index.php
@@ -70,62 +70,74 @@ if (isset($parameters['page'])) {
         exit;
     }
 } else if (isset($parameters['register'])) {
+    
     $db = new PDO($db_connection);
     $sqlInsert = 'INSERT INTO users (user_name, user_password, verification_token, is_verified) VALUES (:user_name, :user_password, :verification_token, 0)';
     $sqlCheck = 'SELECT user_name FROM users WHERE user_name = :user_name';
 
-    
     $username = $parameters['user_name'];
     $password = $parameters['user_password'];
+    
+    $recaptcha_secret = '6LcimFUqAAAAAHEbCaUr43uu795ylas3X2umCGRD';
+    $recaptcha_response = $parameters['g-recaptcha-response'];
 
-    // to verify longitude of password
-    $min_length = 8;
-    $max_length = 128;
-    if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
-        // Si l'usuari no és un correu electrònic vàlid
-        $configuration['{FEEDBACK}'] = '<mark>ERROR: El nom d\'usuari ha de ser una adreça de correu electrònic vàlida.</mark>';
-        $crearUsuari = false;
-    }
-    else{
-        $queryCheck = $db->prepare($sqlCheck);
-        $username=$parameters['user_name'];
-        $queryCheck->bindValue(':user_name', $username);
-        $queryCheck->execute();
-        $result_row = $queryCheck->fetch(PDO::FETCH_ASSOC);
-        if ($result_row){ // si l'usuari existeix
-            $configuration['{FEEDBACK}'] = '<mark>ERROR: L\'usuari ja existeix. Si us plau, escolliu un altre nom d\'usuari.</mark>';
-        }
-        else if (strlen($password) < $min_length || strlen($password) > $max_length) {
-            // Mostrar missatge error si la contrasenya no cumpleix amb la mida correcta
-            $configuration['{FEEDBACK}'] = '<mark>ERROR: La contrasenya ha de tenir entre ' . $min_length . ' i ' . $max_length . ' caràcters</mark>';
+    $recaptcha_verify_url = 'https://www.google.com/recaptcha/api/siteverify?';
+    $recaptcha = file_get_contents($recaptcha_verify_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+    if (!$recaptcha->success) {
+        $configuration['{FEEDBACK}'] = '<mark>ERROR: Verificació de reCAPTCHA fallida. Si us plau, intenta-ho de nou'. $recaptcha_response . '</mark>';
+        $min_length = 8;
+        $max_length = 128;
+        if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            // Si l'usuari no és un correu electrònic vàlid
+            $configuration['{FEEDBACK}'] = '<mark>ERROR: El nom d\'usuari ha de ser una adreça de correu electrònic vàlida.</mark>';
+            $crearUsuari = false;
         }
         else{
-            // HASH PASSWORD
-            $salt = bin2hex(random_bytes(16)); // crear sal aleatoria
-            $hashed_password = hash_pbkdf2('sha256', $password, $salt, 10000, 64); //fer hash
+            $queryCheck = $db->prepare($sqlCheck);
+            $username=$parameters['user_name'];
+            $queryCheck->bindValue(':user_name', $username);
+            $queryCheck->execute();
+            $result_row = $queryCheck->fetch(PDO::FETCH_ASSOC);
+            if ($result_row){ // si l'usuari existeix
+                $configuration['{FEEDBACK}'] = '<mark>ERROR: L\'usuari ja existeix. Si us plau, escolliu un altre nom d\'usuari.</mark>';
+            }
+            else if (strlen($password) < $min_length || strlen($password) > $max_length) {
+                // Mostrar missatge error si la contrasenya no cumpleix amb la mida correcta
+                $configuration['{FEEDBACK}'] = '<mark>ERROR: La contrasenya ha de tenir entre ' . $min_length . ' i ' . $max_length . ' caràcters</mark>';
+            }
+            else{
+                // HASH PASSWORD
+                $salt = bin2hex(random_bytes(16)); // crear sal aleatoria
+                $hashed_password = hash_pbkdf2('sha256', $password, $salt, 10000, 64); //fer hash
 
-            $password_final = $salt . ':' . $hashed_password; // ajuntar amb el format 'sal:hash'
+                $password_final = $salt . ':' . $hashed_password; // ajuntar amb el format 'sal:hash'
 
-            $token = bin2hex(random_bytes(16)); // Genera un token aleatori per verificar a partir del correu
-            
-            $query = $db->prepare($sqlInsert);
-            $query->bindValue(':user_name', $parameters['user_name']);
-            $query->bindValue(':user_password', $password_final);
-            $query->bindValue(':verification_token', $token);
-            if ($query->execute()) {
-                if (sendVerificationEmail($parameters['user_name'], $token)) { //enviar correu de verificació
-                    $configuration['{FEEDBACK}'] = 'Verifica el correu <b>' . htmlentities($parameters['user_name']) . '</b>';
-                    $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Iniciar sessió';
+                $token = bin2hex(random_bytes(16)); // Genera un token aleatori per verificar a partir del correu
+                
+                $query = $db->prepare($sqlInsert);
+                $query->bindValue(':user_name', $parameters['user_name']);
+                $query->bindValue(':user_password', $password_final);
+                $query->bindValue(':verification_token', $token);
+                if ($query->execute()) {
+                    if (sendVerificationEmail($parameters['user_name'], $token)) { //enviar correu de verificació
+                        $configuration['{FEEDBACK}'] = 'Verifica el correu <b>' . htmlentities($parameters['user_name']) . '</b>';
+                        $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Iniciar sessió';
+                    } else {
+                        $configuration['{FEEDBACK}'] = '<mark>ERROR: No s\'ha pogut enviar el correu de verificació.</mark>';
+                    }
                 } else {
-                    $configuration['{FEEDBACK}'] = '<mark>ERROR: No s\'ha pogut enviar el correu de verificació.</mark>';
+                    // Això no s'executarà mai (???)
+                    $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut crear el compte <b>"
+                        . htmlentities($parameters['user_name']) . '</b></mark>';
                 }
-            } else {
-                // Això no s'executarà mai (???)
-                $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut crear el compte <b>"
-                    . htmlentities($parameters['user_name']) . '</b></mark>';
             }
         }
+    } else {
+        $configuration['{FEEDBACK}'] = '<mark>ERROR: Verificació de reCAPTCHA fallida. Si us plau, intenta-ho de nou.</mark>';
     }
+    // to verify longitude of password
+    
 } else if (isset($parameters['login'])) {
     $db = new PDO($db_connection);
     $sql = 'SELECT * FROM users WHERE user_name = :user_name';
